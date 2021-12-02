7538919c8736f9e41f7a99569544e667b7f9c1ea
sebastian <sebastian.englund@knowe.se>
2021-12-02 22:38:45 +0100

    Day1 1.0

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,40 @@
+﻿var input = File.ReadAllLines("./input.txt").Select(x => int.Parse(x)).ToList();
+
+Part2(input);
+
+
+static void part1(IEnumerable<int> input)
+{
+
+    var beforNr = input.First();
+    var counter = 0;
+
+    foreach (var item in input)
+    {
+        if (item > beforNr)
+            counter++;
+
+        beforNr = item;
+    }
+
+    Console.WriteLine(counter);
+    Console.ReadLine();
+
+}
+
+static void Part2(List<int> input)
+{
+    var counter = 0;
+
+    for (int i = 0; i + 2 < input.Count; i++)
+    {
+
+        if (input.GetRange(i, 3).Sum() > input.GetRange(i == 0 ? i : i - 1, 3).Sum())
+            counter++;
+
+    }
+
+    Console.WriteLine(counter);
+    Console.ReadLine();
+
+}
