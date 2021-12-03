6a574b341122f2eb20f2895713bb149f35354aa2
sebastian <sebastian.englund@knowe.se>
2021-12-03 22:31:36 +0100

    Day3 1.0

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,51 @@
+﻿var input = File.ReadAllLines("./input.txt").ToList();
+PartTwo(input);
+
+
+static void PartOne(List<string> input)
+{
+    var gammaRate = "";
+    var epsilonRate = "";
+
+    for (int i = 0; i < input.First().Count(); i++)
+    {
+        if ((input.Count() / 2) > input.Where(x => x[i] == '1').Count())
+        {
+            gammaRate += "0";
+            epsilonRate += "1";
+        }
+        else
+        {
+            gammaRate += "1";
+            epsilonRate += "0";
+        }
+    }
+    Console.WriteLine(Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2));
+    Console.ReadLine();
+}
+
+static void PartTwo(List<string> input)
+{
+    var CO2ScrubberRating = input;
+    var oxygenGeneratorRating = input;
+
+    for (int i = 0; i < input.First().Count(); i++)
+    {
+        if ((oxygenGeneratorRating.Count() / 2) >= oxygenGeneratorRating.Where(x => x[i] == '0').Count() && oxygenGeneratorRating.Count > 1)
+            oxygenGeneratorRating = oxygenGeneratorRating.Where(x => x[i] == '1').ToList();
+        else if (oxygenGeneratorRating.Count > 1)
+            oxygenGeneratorRating = oxygenGeneratorRating.Where(x => x[i] == '0').ToList();
+
+        if ((CO2ScrubberRating.Count() / 2) >= CO2ScrubberRating.Where(x => x[i] == '0').Count() && CO2ScrubberRating.Count > 1)
+            CO2ScrubberRating = CO2ScrubberRating.Where(x => x[i] == '0').ToList();
+        else if (CO2ScrubberRating.Count > 1)
+            CO2ScrubberRating = CO2ScrubberRating.Where(x => x[i] == '1').ToList();
+
+
+        if (CO2ScrubberRating.Count == 1 && oxygenGeneratorRating.Count == 1)
+            break;
+    }
+
+    Console.WriteLine(Convert.ToInt32(CO2ScrubberRating.First(), 2) * Convert.ToInt32(oxygenGeneratorRating.First(), 2));
+    Console.ReadLine();
+}
