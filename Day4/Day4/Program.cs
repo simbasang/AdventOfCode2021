3a51250afd76697ace7236ee829d938fe4c9c5dd
sebastian <sebastian.englund@knowe.se>
2021-12-05 20:35:15 +0100

    day4 base not working

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = File.ReadAllText("./input.txt");
+PartOne(input);
+
+static void PartOne(string input)
+{
+    var bingoRow = input.Split("\n")[0].Split(',').Select(x => int.Parse(x));
+    var boards = input.Replace(input.Split("\n")[0], "").TrimStart().Split("\r\n");
+
+
+    foreach (var item in bingoRow)
+    {
+
+
+    }
+}
